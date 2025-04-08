f8e6d13a9528ea8e583b77bbb3eb296d424cf7ce
joaovic <joaovictorca2004@gmail.com>
2025-04-08 06:37:36 -0300

    aula 06 classes e herancas

--- pyside6/aula04.py
+++ pyside6/aula04.py
@@ -1,7 +1,15 @@
-# O básico sobre Signal e Slots (eventos e documentação)
+# QMainWindow e centralWidget
+# -> QApplication (app)
+#   -> QMainWindow (window->setCentralWidget)
+#       -> CentralWidget (central_widget)
+#           -> Layout (layout)
+#               -> Widget 1 (botao1)
+#               -> Widget 2 (botao2)
+#               -> Widget 3 (botao3)
+#   -> show
+# -> exec
 import sys
 
-from PySide6.QtCore import Slot
 from PySide6.QtWidgets import (
     QApplication,
     QGridLayout,
@@ -16,42 +24,25 @@ central_widget = QWidget()
 window.setCentralWidget(central_widget)
 window.setWindowTitle("Minha janela bonita")
 
-botao = QPushButton("Texto do Botão")
-botao.setStyleSheet("background-color: blue; color: white; font-size: 80px;")
+botao1 = QPushButton("Texto do botão")
+botao1.setStyleSheet("font-size: 80px;")
 
-botao2 = QPushButton("Texto do Botão 2")
-botao2.setStyleSheet("background-color: red; color: white; font-size: 40px;")
+botao2 = QPushButton("Botão 2")
+botao2.setStyleSheet("font-size: 40px;")
 
-botao3 = QPushButton("Texto do Botão 3")
-botao3.setStyleSheet("background-color: green; color: white; font-size: 40px;")
+botao3 = QPushButton("Botão 3")
+botao3.setStyleSheet("font-size: 40px;")
 
 layout = QGridLayout()
 central_widget.setLayout(layout)
 
-layout.addWidget(botao, 1, 1, 1, 1)
+layout.addWidget(botao1, 1, 1, 1, 1)
 layout.addWidget(botao2, 1, 2, 1, 1)
 layout.addWidget(botao3, 3, 1, 1, 2)
 
 
-@Slot()
 def slot_example(status_bar):
-    def inner():
-        status_bar.showMessage("O meu slot foi executado")
-
-    return inner
-
-
-@Slot()
-def outro_slot(checked):
-    print("Está marcado?", checked)
-
-
-@Slot()
-def terceiro_slot(action):
-    def inner():
-        outro_slot(action.isChecked())
-
-    return inner
+    status_bar.showMessage("O meu slot foi executado")
 
 
 # statusBar
@@ -62,14 +53,10 @@ status_bar.showMessage("Mostrar mensagem na barra")
 menu = window.menuBar()
 primeiro_menu = menu.addMenu("Primeiro menu")
 primeira_acao = primeiro_menu.addAction("Primeira ação")
-primeira_acao.triggered.connect(slot_example(status_bar))  # type:ignore
+primeira_acao.triggered.connect(  # type:ignore
+    lambda: slot_example(status_bar)
+)
 
-segunda_action = primeiro_menu.addAction("Segunda ação")
-segunda_action.setCheckable(True)
-segunda_action.toggled.connect(outro_slot)  # type:ignore
-segunda_action.hovered.connect(terceiro_slot(segunda_action))  # type:ignore
-
-botao.clicked.connect(terceiro_slot(segunda_action))  # type:ignore
 
 window.show()
 app.exec()  # O loop da aplicação
